027c721df054bf35022fb9b4638384b23cfac466
simon1357-hash <simon.khan@outlook.fr>
2025-09-22 19:12:12 +0200

    Add files via upload

---
+++ core/context_processor.py
@@ -0,0 +1,21 @@
+from ast import Add
+from core.models import Product, Category, Vendor, CartOrder, ProductImages, ProductReview, wishlist_model, Address
+from django.db.models import Min, Max
+
+def default(request):
+    categories = Category.objects.all()
+    vendors = Vendor.objects.all()
+    
+    min_max_price = Product.objects.aggregate(Min("price"), Max("price"))
+    
+    try:
+        address = Address.objects.get(user=request.user)
+    except:
+        address = None
+    
+    return {
+        'categories':categories,
+        'address':address,
+        'vendors':vendors,
+        'min_max_price':min_max_price,
+    }
---
+++ core/urls.py
@@ -0,0 +1,28 @@
+from django import views
+from django.urls import path, include
+from core.views import index, product_list_view, product_detail_view, category_list_view, category_product_list__view, vendor_list_view, vendor_detail_view, tag_list, ajax_add_review, search_view, filter_product, add_to_cart, cart_view, delete_item_from_cart, update_cart, checkout_view, payment_completed_view, payment_failed_view
+
+
+app_name = "core"
+
+urlpatterns = [
+    path("", index , name="index"),
+    path("products/", product_list_view, name="product-list"),
+    path("product/<pid>/", product_detail_view, name="product-detail"),
+    path("category/", category_list_view, name="category-list"),
+    path("category/<cid>/", category_product_list__view, name="category-product-list"),
+    path("vendors/", vendor_list_view, name="vendor-list"),
+    path("vendor/<vid>/", vendor_detail_view, name="vendor-detail"),
+    path("products/tag/<slug:tag_slug>/", tag_list, name="tags"),
+    path("ajax-add-review/<int:pid>/", ajax_add_review, name="ajax-add-review"),
+    path("search/", search_view, name="search"),
+    path("filter-products/", filter_product, name="filter-product"),
+    path("add-to-cart/", add_to_cart, name="add-to-cart"),
+    path("cart/", cart_view, name="cart"),
+    path("delete-from-cart/", delete_item_from_cart, name="delete-from-cart"),
+    path("update-cart/", update_cart, name="update-cart"),
+    path("checkout/", checkout_view, name="checkout"),
+    path('paypal/', include('paypal.standard.ipn.urls')),
+    path("payment-completed/", payment_completed_view, name="payment-completed"),
+    path("payment-failed/", payment_failed_view, name="payment-failed"),
+]
